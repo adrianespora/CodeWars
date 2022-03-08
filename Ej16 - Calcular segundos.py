d68a8bd9dd73b29c2ded288d413f330820d312b9
AdrianEspora <71982771+adrianespora@users.noreply.github.com>
2022-03-08 19:54:04 -0300

    Update Ej16 - Calcular segundos.py

--- Ej16 - Calcular segundos.py
+++ Ej16 - Calcular segundos.py
@@ -1,8 +1,5 @@
-"""
-Formato de duración legible por humanos
-61 segs = 1 min, 1 seg
-"""
 def format_duration(seconds):
+    segundos_resultado = seconds
     cadena = ''
     dias = []
     horas =[]
@@ -17,27 +14,27 @@ def format_duration(seconds):
     for x in range(86400,31536000,86400):
         dias.append(x)
     if seconds == 0:
-        cadena = cadena + 'now'
+        cadena = cadena + 'Ahora'
     elif seconds == 1:
-        cadena = cadena + '1 second'
+        cadena = cadena + '1 segundo'
         """ AÑOS """
     else:
         if seconds >= 31536000 and seconds < (31536000*2): # si los segundos estan entre esos valores es 1 año
-            cadena = cadena + '1 year'
+            cadena = cadena + '1 año'
             resto = seconds - 31536000*(int(seconds/31536000)) #guardo en resto los segundos que estan entre 1 año y 2
             if resto > 0:
                 if resto in dias or resto in horas or resto in minutos or resto in segundos:
                         seconds = resto
-                        cadena = cadena + ' and '
+                        cadena = cadena + ' y '
                 else:
                     cadena = cadena + ', ' # como RESTO es mayor agrego ', '
                     seconds = resto # almaceno en SECONDS el RESTO
         elif seconds >= 31536000*2: # si es mayor a 2 años
-            cadena = cadena + str(int(seconds/31536000)) + ' years'
+            cadena = cadena + str(int(seconds/31536000)) + ' años'
             resto = seconds - 31536000*(int(seconds/31536000))
             if resto in dias or resto in horas or resto in minutos or resto in segundos:
                 seconds = resto
-                cadena = cadena + ' and '
+                cadena = cadena + ' y '
             else:
                 cadena = cadena + ', ' # como RESTO es mayor agrego ', '
                 seconds = resto # almaceno en SECONDS el RESTO
@@ -45,21 +42,21 @@ def format_duration(seconds):
 
         """ DIAS """
         if seconds >= 86400 and seconds < 172800: # si los segundos estan entre esos valores es 1 dia
-            cadena = cadena + '1 day'
+            cadena = cadena + '1 dia'
             resto = seconds - 86400*(int(seconds/86400)) # guardo en RESTO los segundos que estan entre 1 dia y 2
             if resto > 0:
                 if resto in horas or resto in minutos or resto in segundos:
                     seconds = resto
-                    cadena = cadena + ' and '
+                    cadena = cadena + ' y '
                 else:
                     cadena = cadena + ', ' # como RESTO es mayor agrego ', '
                     seconds = resto # almaceno en SECONDS el RESTO
         elif seconds >= 172800 and seconds < 31536000: # si es mayor a 2 dias
-            cadena = cadena + str(int(seconds/86400)) + ' days'
+            cadena = cadena + str(int(seconds/86400)) + ' dias'
             resto = seconds - 86400*(int(seconds/86400))
             if resto in horas or resto in minutos or resto in segundos:
                 seconds = resto
-                cadena = cadena + ' and '
+                cadena = cadena + ' y '
             elif resto == 0:
                 pass
             else:
@@ -68,21 +65,21 @@ def format_duration(seconds):
                 
         """ HORAS """
         if seconds >= 3600 and seconds < 7200: # si los segundos estan entre esos valores es 1 dia
-            cadena = cadena + '1 hour'
+            cadena = cadena + '1 hora'
             resto = seconds - 3600*(int(seconds/3600)) # guardo en RESTO los segundos que estan entre 1 dia y 2
             if resto > 0:
                 if resto in minutos or resto in segundos:
                     seconds = resto
-                    cadena = cadena + ' and '
+                    cadena = cadena + ' y '
                 else:
                     cadena = cadena + ', ' # como RESTO es mayor agrego ', '
                     seconds = resto # almaceno en SECONDS el RESTO
         elif seconds >= 7200 and seconds < 86400: # si es mayor a 2 dias
-            cadena = cadena + str(int(seconds/3600)) + ' hours'
+            cadena = cadena + str(int(seconds/3600)) + ' horas'
             resto = seconds - 3600*(int(seconds/3600))
             if resto in minutos or resto in segundos:
                 seconds = resto
-                cadena = cadena + ' and '
+                cadena = cadena + ' y '
             elif resto == 0:
                 pass
             else:
@@ -91,21 +88,21 @@ def format_duration(seconds):
 
         """ MINUTOS """
         if seconds >= 60 and seconds < 120: # si los segundos estan entre esos valores es 1 dia
-            cadena = cadena + '1 minute'
+            cadena = cadena + '1 minuto'
             resto = seconds - 60*(int(seconds/60)) # guardo en RESTO los segundos que estan entre 1 dia y 2
             if resto > 0:
                 if resto in segundos:
                     seconds = resto
-                    cadena = cadena + ' and '
+                    cadena = cadena + ' y '
                 else:
                     cadena = cadena + ', ' # como RESTO es mayor agrego ', '
                     seconds = resto # almaceno en SECONDS el RESTO
         elif seconds >= 120 and seconds < 3600: # si es mayor a 2 dias
-            cadena = cadena + str(int(seconds/60)) + ' minutes'
+            cadena = cadena + str(int(seconds/60)) + ' minutos'
             resto = seconds - 60*(int(seconds/60))
             if resto in segundos:
                 seconds = resto
-                cadena = cadena + ' and '
+                cadena = cadena + ' y '
             elif resto == 0:
                 pass
             else:
@@ -115,56 +112,37 @@ def format_duration(seconds):
                 
         """ SEGUNDOS """
         if seconds == 1:
-            cadena = cadena + '1 second'
+            cadena = cadena + '1 segundo'
         elif seconds >1 and seconds < 60:
-            cadena = cadena + str(int(seconds)) + ' seconds'
+            cadena = cadena + str(int(seconds)) + ' segundos'
         
     
-    print(cadena)
-
-año = 31536000
-dia = 86400
-hora = 3600
-minuto = 60
-UnMin_DosSeg = 62
-DosMin_Dos_Seg = 122
-año_dia = 31536000+86400
-año_2dias = 31536000+(86400*2)
-año_dia_hora = 31536000+86400+3600
-año_dia_hora_min = 31536000+86400+3600+60
-año_dia_hora_min_seg = 31536000+86400+3600+60+40
-numero = 92536000+126400+8900+60+5
-años_dias_horas_minutos_segundos = año_dia_hora_min_seg*2
-DosAños_UnDia = (31536000*2)+86400
-DosAños_DosDias = (31536000*2)+(86400*2)
-DosAños_DosDias_UnaHora = (31536000*2)+(86400*2)+3600
-DosAños_DosDias_DosHoras_UnMinuto = (31536000*2)+(86400*2)+(3600*2)+60
-
-format_duration(año_2dias)
-format_duration(DosAños_DosDias)
-
-format_duration(año)
-format_duration(dia)
-format_duration(hora)
-format_duration(minuto)
-format_duration(UnMin_DosSeg)
-format_duration(DosMin_Dos_Seg)
-format_duration(año_dia)
-format_duration(año_2dias)
-format_duration(año_dia_hora)
-format_duration(año_dia_hora_min)
-format_duration(año_dia_hora_min_seg)
-format_duration(años_dias_horas_minutos_segundos)
-format_duration(DosAños_UnDia)
-format_duration(DosAños_DosDias_UnaHora)
-format_duration(DosAños_DosDias_DosHoras_UnMinuto)
+    print(f'\nLos {segundos_resultado} segundos representan: {cadena}\n')
 
 
 
-"""
-test.assert_equals(format_duration(1), "1 second")
-test.assert_equals(format_duration(62), "1 minute and 2 seconds")
-test.assert_equals(format_duration(120), "2 minutes")
-test.assert_equals(format_duration(3600), "1 hour")
-test.assert_equals(format_duration(3662), "1 hour, 1 minute and 2 seconds")
-"""
+def menu():
+    while True: 
+        print("""CONVERSOR DE SEGUNDOS\n\n
+        1 - Ingresar Segundos\n
+        2 - Salir\n
+        """)
+        try:
+            opcion = int(input("Ingrese una opcion: "))
+        except:
+            print('Hay un error, debe ingresar 1 o 2!\n')
+            menu()
+        try:    
+            if opcion == 1:
+                seconds = int(input('Ingrese los segundos: '))
+                format_duration(seconds) #para referir la variable 'seconds' a otra funcion
+            elif opcion == 2:
+                break
+        except:
+            print('Hay un error, debe ingresar un Numero entero!\n')
+            seconds = int(input('Ingrese los segundos: '))
+            format_duration(seconds) #para referir la variable 'seconds' a otra funcion
+    
+    return
+    
+menu()
